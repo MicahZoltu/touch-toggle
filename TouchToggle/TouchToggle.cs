892c33a43a37b6c9ccae3daa7637edfda5d8b3d3
Micah Caldwell <micah@zoltu.net>
2011-08-04 11:27:51 -0700

    Initial comit.

---
+++ TouchToggle/TouchToggle.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace TouchToggle
+{
+	public partial class MainForm : Form
+	{
+		private NotifyIcon mNotifyIcon;
+		private ContextMenu mContextMenu;
+		
+		private Icon mIconEnabled;
+		private Icon mIconDisabled;
+
+		[STAThread]
+		public static void Main()
+		{
+			Application.Run(new MainForm());\\\\
+		}
+
+		public MainForm()
+		{
+			mContextMenu = new ContextMenu();
+
+			// Initialize the context menu
+			mContextMenu.MenuItems.Add("E&xit", mMenuExit_Click);
+
+			// Set up how the form should be displayed.
+			Visible = false;
+			ShowInTaskbar = false;
+			WindowState = FormWindowState.Minimized;
+
+			// Create the NotifyIcon.
+			mNotifyIcon = new NotifyIcon();
+
+			// The Icon property sets the icon that will appear in the systray for this application.
+			mIconEnabled = new Icon(Icons.Green_Hand, 40, 40);
+			mIconDisabled = new Icon(Icons.Red_Hand, 40, 40);
+			mNotifyIcon.Icon = mIconEnabled;
+
+			// The ContextMenu property sets the menu that will appear when the systray icon is right clicked.
+			mNotifyIcon.ContextMenu = mContextMenu;
+
+			// The Text property sets the text that will be displayed, in a tooltip, when the mouse hovers over the systray icon.
+			mNotifyIcon.Text = "Touch Toggle";
+			mNotifyIcon.Visible = true;
+
+			// Handle the click event.
+			mNotifyIcon.Click += new EventHandler(mNotifyIcon_Click);
+		}
+
+		protected override void Dispose(bool pDisposing)
+		{
+			if (pDisposing && mNotifyIcon != null) mNotifyIcon.Dispose();
+			base.Dispose(pDisposing);
+		}
+
+		private void mMenuExit_Click(Object pSender, EventArgs pEventArgs)
+		{
+			Close();
+		}
+
+		private void mNotifyIcon_Click(Object pSender, EventArgs pEventArgs)
+		{
+			if (ToggleTouchMode()) mNotifyIcon.Icon = mIconEnabled;
+			else mNotifyIcon.Icon = mIconDisabled;
+		}
+
+		private Boolean ToggleTouchMode()
+		{
+			RegistryKey lKey = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Wisp\Touch", true);
+			Int32 lPreviousValue = (Int32)lKey.GetValue("TouchGate");
+			if (lPreviousValue == 0)
+			{
+				lKey.SetValue("TouchGate", 1, RegistryValueKind.DWord);
+				User32Utils.Notify_SettingChange();
+				lKey.Close();
+				return true;
+			}
+			else
+			{
+				lKey.SetValue("TouchGate", 0, RegistryValueKind.DWord);
+				User32Utils.Notify_SettingChange();
+				lKey.Close();
+				return false;
+			}
+		}
+	}
+}
---
+++ TouchToggle/User32Utils.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace TouchToggle
+{
+	internal class User32Utils
+	{
+		#region USER32 Options
+		static IntPtr HWND_BROADCAST = new IntPtr(0xffffL);
+		static IntPtr WM_SETTINGCHANGE = new IntPtr(0x1a);
+		#endregion
+
+		#region STRUCT
+		enum SendMessageTimeoutFlags : uint
+		{
+			SMTO_NORMAL = 0x0000,
+			SMTO_BLOCK = 0x0001,
+			SMTO_ABORTIFHUNG = 0x2,
+			SMTO_NOTIMEOUTIFNOTHUNG = 0x0008
+		}
+		#endregion
+
+		#region Interop
+
+		[DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+		static extern IntPtr SendMessageTimeout(IntPtr hWnd, uint Msg, UIntPtr wParam, UIntPtr lParam, SendMessageTimeoutFlags fuFlags, uint uTimeout, out UIntPtr lpdwResult);
+		#endregion
+
+		internal static void Notify_SettingChange()
+		{
+			UIntPtr result;
+			SendMessageTimeout(HWND_BROADCAST, (uint)WM_SETTINGCHANGE, UIntPtr.Zero, UIntPtr.Zero, SendMessageTimeoutFlags.SMTO_NORMAL, 1000, out result);
+		}
+	}
+}
